7c8b05ffa0b3963e813d0e28522d53ac276b48bb
SAI SUNIL AKURATHI <saisunilakurathi@gmail.com>
2022-10-04 14:29:23 -0400

    running webserver(express) using node.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const server = express();
+
+const PORT = 8080;
+
+server.get('/', (req, resp)=>{
+    resp.send("<h1>Hello World!!!</h1>")
+});
+
+server.get('/firstView', (req, resp)=>{
+    resp.send("<h1>Welcome to FirstView!</h1>" + "\n" + "<p style='color:red;'>Example of node app using webserver from first view page</p>");
+});
+
+server.get('/secondView', (req, resp)=>{
+    resp.send("<h1>Welcome to secondView!</h1>" + "\n" + "<p style='color:red;'>Example of node app using webserver from second view page</p>");
+}).listen(PORT, ()=>{
+    console.log('Server started and running on ' + PORT);
+});
